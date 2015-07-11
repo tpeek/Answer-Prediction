b59f5c23b5b99b34e69243f9389189814ca3fbd7
wesleywooten <wootenwesley@gmail.com>
2015-07-10 18:13:08 -0700

    added login views and user class

---
+++ app.py
@@ -0,0 +1,69 @@
+import os
+
+from pyramid.httpexceptions import HTTPFound
+from pyramid.view import view_config
+
+from pyramid.security import remember, forget
+from cryptacular.bcrypt import BCRYPTPasswordManager
+
+import sqlalchemy as sa
+from sqlalchemy.orm import scoped_session, sessionmaker
+from zope.sqlalchemy import ZopeTransactionExtension
+from sqlalchemy.ext.declarative import declarative_base
+
+DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
+DATABASE_URL = os.environ.get(
+    'DATABASE_URL',
+    ""  # TODO update once database is set up
+)
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    username = sa.Column(sa.Unicode(127), nullable=False, unique=True)
+    password = sa.Column(sa.Unicode(127), nullable=False)
+
+    @classmethod
+    def get_by_id(cls, id, session=DBSession):
+        return session.query(cls).filter(cls.id == id).one()
+
+    @classmethod
+    def get_by_username(cls, username, session=DBSession):
+        return session.query(cls).filter(cls.username == username).one()
+
+
+@view_config(route_name="login_page", renderer="string")
+def login_page(request):
+    username = request.params.get('username', '')
+    error = ""
+    if request.method == "POST":
+        try:
+            authenticated = login(request)
+        except ValueError as e:
+            error = e
+
+        if authenticated:
+            headers = remember(request, username)
+            return HTTPFound(request.route_url('home'), headers=headers)
+    return {'error': error}
+
+
+def login(request):
+    username = request.params.get("username", None)
+    password = request.params.get("password", None)
+    if not (username and password):
+        raise ValueError("Username and password are required")
+    manager = BCRYPTPasswordManager()
+    try:
+        user = User.get_by_username(username)
+    except:
+        raise ValueError("User does not exist")
+    return manager.check(user.password, password)
+
+
+@view_config(route_name="home", renderer="text")
+def home(request):
+    return "You are at the home page"
